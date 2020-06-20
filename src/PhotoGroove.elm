8f6da00c440cc1a36381b454d4f409caaa0ac9dd
wangonya <kwangonya@gmail.com>
2020-06-20 16:44:31 +0300

    first page render

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,1 +1,18 @@
-module PhotoGroove exposing (..)
+module PhotoGroove exposing (main)
+
+import Html exposing (div, h1, img, text)
+import Html.Attributes exposing (..)
+
+
+view model =
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , div [ id "thumbnails" ]
+            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
+            , img [ src "http://elm-in-action.com/2.jpeg" ] []
+            ]
+        ]
+
+
+main =
+    view "no model yet"
